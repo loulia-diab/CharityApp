71c3e24e4f383a5ee07e94020e5215ecd09af72f
loulia-diab <louliadiab.2004@gmail.com>
2025-08-19 00:35:30 +0300

    modify

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,18 +13,15 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['main_category'=>'Campaign','name_category_en' => 'All',         'name_category_ar' => 'الكل'],
             ['main_category'=>'Campaign','name_category_en' => 'Construction', 'name_category_ar' => 'بناء'],
             ['main_category'=>'Campaign','name_category_en' => 'Health',       'name_category_ar' => 'صحة'],
             ['main_category'=>'Campaign','name_category_en' => 'Education',    'name_category_ar' => 'تعليم'],
             //
-            ['main_category'=>'HumanCase','name_category_en' => 'All',         'name_category_ar' => 'الكل'],
             ['main_category'=>'HumanCase','name_category_en' => 'Patients',        'name_category_ar' => 'مرضى'],
             ['main_category'=>'HumanCase','name_category_en' => 'Student',         'name_category_ar' => 'طالب علم'],
             ['main_category'=>'HumanCase','name_category_en' => 'Needy Families',  'name_category_ar' => 'أسر متعففة'],
 
             //
-            ['main_category'=>'Sponsorship','name_category_en' => 'All',         'name_category_ar' => 'الكل'],
             ['main_category'=>'Sponsorship','name_category_en' => 'Orphan',        'name_category_ar' => 'يتيم'],
             ['main_category'=>'Sponsorship','name_category_en' => 'Poor Families', 'name_category_ar' => 'أسر فقيرة'],
             ['main_category'=>'Sponsorship','name_category_en' => 'Student',       'name_category_ar' => 'طالب علم'],
